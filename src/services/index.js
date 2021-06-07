df60bce03379b007e9cf30d21e1f198d04a3c22a
Jyotsana Khannu <jyotsana.khannu@empower-solutions.co.in>
2021-06-07 14:35:08 +0530

    Second Commit

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
+  console.log('app',app);
   app.configure(users);
 };
